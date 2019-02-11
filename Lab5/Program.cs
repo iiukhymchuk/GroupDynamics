8ee56332ac720cdd1b5970dd43af3c7741f6e0b2
iiukhymchuk <ievgenii.iukhymchuk@gmail.com>
2019-02-11 21:48:03 +0200

    Add Lab 5.

---
+++ Lab5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Utilities;
+
+namespace Lab5
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string text = FileHelpers.ReadFile("Select file to count symbols");
+
+            var allSymbols = text.Length;
+            var numberOfLetters = text.Count(char.IsLetter);
+            var numberOfNonLetters = allSymbols - numberOfLetters;
+
+            var textToSave = $"Number of all symbols is {allSymbols}, letters - {numberOfLetters}, non letters - {numberOfNonLetters}.";
+
+            Console.WriteLine(textToSave);
+
+            FileHelpers.SaveFile(textToSave, "Save result file");
+        }
+    }
+}
